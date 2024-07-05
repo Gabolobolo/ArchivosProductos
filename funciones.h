52718c1ecda58a4c614efe3351c108c57218a37e
Gabolobolo <136251040+Gabolobolo@users.noreply.github.com>
2024-07-05 15:19:44 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -5,3 +5,5 @@ void buscarXPrecioMenor(char productos[][3][40],double precio[]);
 void editarProducto(char productos[][3][40],double precio[], int indice[]);
 void leerProductos(char productos[][3][40], double precio[], int indice[]);
 void guardarProductos(char productos[][3][40], double precio[], int indice[]);
+void anadirproducto(char productos[][3][40], double precio[], int indice[]);
+void eliminarproducto(char productos[][3][40], double precio[], int indice[]);
