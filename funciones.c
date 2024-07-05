0081effcffc425cb215ca65d8b10fcfaf7e8f59e
Gabolobolo <136251040+Gabolobolo@users.noreply.github.com>
2024-07-05 15:20:30 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -4,20 +4,20 @@
 
 void listarProductos(char productos[][3][40],double precio[],int indice[]){
     printf("Numero\tNombre\t\tCategoria\tMarca\t\tPrecio\n");
-    for (int i=0; i<10; i++)
-    {
-    printf("%d\t\t%s\t\t%s\t\t%s\t\t%.2lf\n", indice[i], productos[i][0],productos[i][1], productos[i][2], precio[i]);
+    for (int i = 0; i < 12; i++) {
+        if (indice[i] != 0) { 
+            printf("%d\t\t%s\t\t%s\t\t%s\t\t%.2lf\n", indice[i], productos[i][0],productos[i][1], productos[i][2], precio[i]);
+        }
     }
 }
 
-//Comparar (buscar)//
 void buscarXMarca(char productos[][3][40],double precio[]){
     char marca[40];
     int noexiste = 0;
     printf("Ingrese la marca: ");
     scanf("%s",marca);
     printf("Nombre\t\tCategoria\tMarca\t\tPrecio\n");
-    for (int i = 0; i<10; i++)
+    for (int i = 0; i<20; i++)
     {
         if(strcmp(productos[i][2],marca)==0)
         {
@@ -57,7 +57,7 @@ void buscarXPrecioMenor(char productos[][3][40],double precio[]){
     printf("Ingrese el precio maximo: ");
     scanf("%lf",&preciomaximo);
     printf("Nombre\t\tCategoria\tMarca\t\tPrecio\n");
-    for (int i = 0; i<10; i++)
+    for (int i = 0; i<20; i++)
     {
         if(precio[i]<=preciomaximo)
         {
@@ -97,7 +97,7 @@ void leerProductos(char productos[][3][40], double precio[], int indice[]) {
         printf("Error al abrir el archivo 'productos.txt'\n");
         return;
     }
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         fscanf(fp, "%d %s %s %s %lf\n", &indice[i], productos[i][0], productos[i][1], productos[i][2], &precio[i]);
     }
     fclose(fp);
@@ -109,8 +109,69 @@ void guardarProductos(char productos[][3][40], double precio[], int indice[]) {
         printf("Error al abrir el archivo 'productos.txt'\n");
         return;
     }
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         fprintf(fp, "%d %s %s %s %.2lf\n", indice[i], productos[i][0], productos[i][1], productos[i][2], precio[i]);
     }
     fclose(fp);
+}
+
+void anadirproducto(char productos[][3][40], double precio[], int indice[]) {
+    char nombre[40], categoria[40], marca[40];
+        double nuevoPrecio;
+        int nuevafila = 10; 
+        while (nuevafila >= 0 && indice[nuevafila] != 0) {
+            nuevafila--;
+        }
+        if (nuevafila < 0) {
+            printf("No hay espacio disponible para agregar un nuevo producto.\n");
+            return;
+        }
+        printf("Ingrese el nombre del producto: ");
+        scanf("%s", nombre);
+        printf("Ingrese la categoria del producto: ");
+        scanf("%s", categoria);
+        printf("Ingrese la marca del producto: ");
+        scanf("%s", marca);
+        printf("Ingrese el precio del producto: ");
+        scanf("%lf", &nuevoPrecio);
+        for (int i = 9; i > nuevafila; i--) { 
+            strcpy(productos[i][0], productos[i - 1][0]);
+            strcpy(productos[i][1], productos[i - 1][1]);
+            strcpy(productos[i][2], productos[i - 1][2]);
+            precio[i] = precio[i - 1];
+            indice[i] = indice[i - 1]; 
+        }
+        strcpy(productos[nuevafila][0], nombre);
+        strcpy(productos[nuevafila][1], categoria);
+        strcpy(productos[nuevafila][2], marca);
+        precio[nuevafila] = nuevoPrecio;
+        indice[nuevafila] = nuevafila + 1; 
+        printf("Producto agregado exitosamente!\n");
+        listarProductos(productos, precio, indice); 
+    }
+
+void eliminarproducto(char productos[][3][40], double precio[], int indice[]) {
+    int idToRemove, i, j;
+    printf("Ingrese el ID del producto que desea eliminar: ");
+    scanf("%d", &idToRemove);
+    for (i = 0; i < 10; i++) {
+        if (indice[i] == idToRemove) {
+            for (j = i; j < 9; j++) { 
+                strcpy(productos[j][0], productos[j + 1][0]);
+                strcpy(productos[j][1], productos[j + 1][1]);
+                strcpy(productos[j][2], productos[j + 1][2]);
+                precio[j] = precio[j + 1];
+                indice[j] = indice[j + 1] - 1; 
+            }
+            strcpy(productos[9][0], "");
+            strcpy(productos[9][1], "");
+            strcpy(productos[9][2], "");
+            precio[9] = 0.0;
+            indice[9] = 0; 
+            printf("Producto eliminado exitosamente!\n");
+            listarProductos(productos, precio, indice); 
+            return; 
+        }
+    }
+    printf("No se encontro un producto con el ID %d\n", idToRemove);
 }
